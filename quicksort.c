ce7dec3c0dd2fa76a7e517dac03caa8f96b15f21
Lab² <93746225+LeoLabbe@users.noreply.github.com>
2023-09-25 17:26:18 -0300

    Quicksort atualizado

--- quicksort.c
+++ quicksort.c
@@ -1,35 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int particionar(int array[], int esq, int dir)
+typedef struct vector{
+	int totalTam;
+	int totalElements;
+	int* array;
+} Vector;
+
+
+Vector* iniciarVector() {
+	Vector* newVec = (Vector*) malloc(sizeof(Vector));
+	
+	newVec -> totalTam = 2;
+	newVec -> totalElements = 0;
+	newVec -> array = (int*) malloc(2 * sizeof(int));
+
+    return newVec;
+}
+
+void realocaVec(Vector* currVec) {
+
+	currVec -> totalTam *= 2;
+	currVec -> array = (int*) realloc(currVec -> array, currVec -> totalTam * sizeof(int));
+
+}
+
+void addElement(Vector* currVec, int num) {
+
+	if(currVec -> totalTam == currVec -> totalElements)
+		realocaVec(currVec);
+
+	currVec -> array[currVec -> totalElements] = num;
+	currVec -> totalElements += 1;
+}
+
+int particionar(Vector* array, int esq, int dir)
 {
     int aux;
     int cont = esq;
+    int i;
 
-        for (int i = esq +1; i <= dir; i++)
+        for (i = esq +1; i <= dir; i++)
          {
-            if(array[i] < array[esq]){
+            if(array -> array[i] < array -> array[esq]){
             cont++;
-            aux = array[i];
-            array[i] = array[cont];
-            array[cont] = aux;
+            aux = array-> array[i];
+           array -> array[i] = array -> array[cont];
+            array -> array[cont] = aux;
             }
 
         }
 
-    aux = array[esq];
-    array[esq] = array[cont];
-    array[cont] = aux;
-
+    aux = array -> array[esq];
+    array -> array[esq] = array -> array[cont];
+    array -> array[cont] = aux;
 
     return cont;
 }
+int particiona_r(Vector* array, int esq, int dir)
+{
+    // Gera um numero aleatório entre
+    // esq .. dir
+    srand(time(NULL));
+    int random = esq + (rand() % (dir - esq));
+ 
+    int aux = array -> array[random];
+    array -> array[random] =  array -> array[esq];
+    array -> array[esq] = aux;
+ 
+    return particionar(array, esq, dir);
+}
 
-void quicksort(int array[], int esq, int dir)
+void quicksort(Vector* array, int esq, int dir)
 {
     int pos;
     if(esq<dir) {
-        pos = particionar(array, esq, dir);
+        pos = particiona_r(array, esq, dir);
         quicksort(array, esq, pos -1);
         quicksort(array, pos +1, dir);
 
@@ -37,17 +84,33 @@ void quicksort(int array[], int esq, int dir)
 
 }
 
-int main (){
 
-    int array[] = {5, 6, 3, 2, 1, 11, 23, 8, 42, 15};
-    int tam = 10;
-    quicksort(array, 0, tam);
 
-    for(int i = 0; i < 11; i++)
-    {
-        printf("%d", array[i]);
+void printArray(Vector* array, int tam)
+{
+    int i;
+    for(i = 0; i < tam; i++)
+    { 
+        printf("%d", array -> array[i]);
         printf("\n");
     }
+}
 
+
+int main()
+{
+
+    Vector* array;
+    array = iniciarVector();
+    int input;
+
+    while(!feof(stdin)){
+	 	scanf("%d", &input);
+		addElement(array, input);
+	}
+     
+    quicksort(array, 0, array -> totalElements - 1);
+    printArray(array, array -> totalElements);
+     
     return 0;
 }
